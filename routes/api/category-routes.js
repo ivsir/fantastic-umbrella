21d55446d83804405468c72323433c787b7da8ea
Risvi-Tareq <ivsirqerat@gmail.com>
2023-01-05 21:45:41 -0800

    fixed the get categories by id route and the delete category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,11 +24,7 @@ router.get('/:id', async (req, res) => {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with that id!' });
-      return;
-    }
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -67,10 +63,10 @@ router.delete('/:id', async(req, res) => {
       }
     });
 
-    if (!category) {
-      res.status(404).json({ message: 'No category found with this id!' });
-      return;
-    }
+    // if (!category) {
+    //   res.status(404).json({ message: 'No category found with this id!' });
+    //   return;
+    // }
 
     res.status(200).json(categoryData);
   } catch (err) {
